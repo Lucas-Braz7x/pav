287a791405be1c62d87ce8ef00018d6b160a4741
ldutra <ldutra@rpc.com.br>
2024-05-30 21:32:32 -0300

    alterando nome do banco de dados

--- init.py
+++ init.py
@@ -5,7 +5,7 @@ from src.routes.index import initialize_routes
 from db.connect import create_connect_db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clinic.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 create_connect_db(app)
